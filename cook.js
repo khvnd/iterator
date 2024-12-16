7d5ecf501cb809f5b2ad4a75b09532fad5f4dd2c
Nadezhda Kharitonova <blackwolf9283@gmail.com>
2024-12-16 13:24:11 +0600

    Добавляем заказы клиентов

--- cook.js
+++ cook.js
@@ -14,3 +14,12 @@ const dishes = new Map([
 ]);
 
 const clients = new Map();
+
+const clientAlexey = { name: "Алексей" };
+clients.set(clientAlexey, ["Пицца \"Пепперони\"", "Тирамису"]);
+
+const clientMaria = { name: "Мария" };
+clients.set(clientMaria, ["Суши \"Калифорния\"", "Пицца \"Маргарита\""]);
+
+const clientIrina = { name: "Ирина" };
+clients.set(clientIrina, ["Чизкейк"]);
